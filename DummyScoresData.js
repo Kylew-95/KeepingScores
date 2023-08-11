5700f2fb065958ed7de8a57880bb4f74c7d21e89
Kyle <kylejordanwilliams@gmail.com>
2023-08-11 20:30:23 +0100

    Added and displayed dummy data wrapped in react paper card components

---
+++ DummyScoresData.js
@@ -0,0 +1,62 @@
+export const data = [
+  {
+    id: 1,
+    Location: "London",
+    activity: "Basketball",
+    gameRound: 2,
+    players: [
+      { player1: "John", scores: 6 },
+      { player2: "Sarah", scores: 6 },
+    ],
+  },
+  {
+    id: 2,
+    Location: "London",
+    activity: "Basketball",
+    gameRound: 2,
+    players: [
+      { player1: "Harry", scores: 6 },
+      { player2: "Sarah", scores: 6 },
+    ],
+  },
+  {
+    id: 3,
+    Location: "London",
+    activity: "Basketball",
+    gameRound: 2,
+    players: [
+      { player1: "John", scores: 6 },
+      { player2: "Sarah", scores: 6 },
+    ],
+  },
+  {
+    id: 4,
+    Location: "London",
+    activity: "Basketball",
+    gameRound: 2,
+    players: [
+      { player1: "John", scores: 6 },
+      { player2: "Sarah", scores: 6 },
+    ],
+  },
+  {
+    id: 5,
+    Location: "London",
+    activity: "Basketball",
+    gameRound: 2,
+    players: [
+      { player1: "John", scores: 6 },
+      { player2: "Sarah", scores: 6 },
+    ],
+  },
+  {
+    id: 6,
+    Location: "London",
+    activity: "Basketball",
+    gameRound: 2,
+    players: [
+      { player1: "John", scores: 6 },
+      { player2: "Sarah", scores: 6 },
+    ],
+  },
+];
