f58861d04ce5fab1d2a0359ba016c965d796235e
Kyle <kylejordanwilliams@gmail.com>
2023-09-06 21:23:25 +0100

    Filtered locations is now mapped

--- Components/MapCarousel.js
+++ Components/MapCarousel.js
@@ -1,43 +1,35 @@
 import { Dimensions, View, Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
-import Carousel, { CarouselProps } from "react-native-snap-carousel"; 
+import Carousel from "react-native-snap-carousel";
 import React from "react";
 
-type Place = {
-  name?: string;
-  vicinity?: string;
-  rating?: string;
-  photos: { photo_reference: string }[];
-}
-
-interface MapCarouselProps {
-  places: Place[];
-  onCarouselItemChange: (place: Place) => void;
-}
-
-const renderItem: CarouselProps<Place>['renderItem'] = ({ item, index }) => { 
-  const photoUrl = item?.photos && item.photos.length > 0
-  ? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=500&photoreference=${item.photos[0].photo_reference}&key=${process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY}`
-  : '';
+const renderItem = ({ item, index }) => {
+  const photoUrl =
+    item.photos && item.photos.length > 0
+      ? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=500&photoreference=${item.photos[0].photo_reference}&key=${process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY}`
+      : "";
 
   return (
     <View style={[styles.slide]}>
       <Card style={styles.card}>
         <Card.Cover
-          style={{ height: 200, resizeMode: "contain", borderRadius: 10,
-          width: 300,
-         }}
+          style={{
+            height: 200,
+            resizeMode: "contain",
+            borderRadius: 10,
+            width: 300,
+          }}
           source={{
             uri: photoUrl,
           }}
         />
         <Card.Content>
-          <Text style={styles.title}>{item?.name || "No Name"}</Text>
+          <Text style={styles.title}>{item.name || "No Name"}</Text>
           <Text style={styles.description}>
-            {item?.vicinity || "No Address"}
+            {item.vicinity || "No Address"}
           </Text>
           <Text style={styles.description}>
-            Rating: {item?.rating || "No Rating"}
+            Rating: {item.rating || "No Rating"}
           </Text>
         </Card.Content>
       </Card>
@@ -45,10 +37,7 @@ const renderItem: CarouselProps<Place>['renderItem'] = ({ item, index }) => {
   );
 };
 
-export default function MapCarousel({
-  places,
-  onCarouselItemChange,
-}: MapCarouselProps) {
+export default function MapCarousel({ places, onCarouselItemChange }) {
   const sliderWidth = Dimensions.get("window").width;
   const itemWidth = Dimensions.get("window").width - 60;
 
@@ -87,7 +76,6 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 16,
-
     color: "white",
   },
   description: {
--- Components/Maps.js
+++ Components/Maps.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from "react";
 import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
-import { StyleSheet, View, Dimensions } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Dimensions,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 import { Searchbar } from "react-native-paper";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import * as Location from "expo-location";
 import MapCarousel from "./MapCarousel";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Maps({
   profileData,
@@ -31,14 +36,16 @@ export default function Maps({
     }
   };
 
+  const apiKey = process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY;
+
   const handleGooglePlaceSelect = async (data) => {
     try {
-      const placeDetailsUrl = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${data.place_id}&key=AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY`;
+      const placeDetailsUrl = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${data.place_id}&key=${apiKey}`;
       const response = await fetch(placeDetailsUrl);
       const placeDetailsData = await response.json();
 
       if (placeDetailsData.status === "OK") {
-        const selectedLocation = placeDetailsData.result.geometry.location;
+        const selectedLocation = placeDetailsData.result[0].geometry.location;
         console.log("Selected Location:", selectedLocation);
 
         setMapRegion({
@@ -47,8 +54,6 @@ export default function Maps({
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         });
-
-        setPlaces([]);
       }
     } catch (error) {
       console.error("Error fetching place details:", error);
@@ -56,7 +61,6 @@ export default function Maps({
 
     setSearchQuery("");
   };
-
   useEffect(() => {
     const fetchNearbyPlaces = async () => {
       const keyWords = ["leisure", "gym", "park", "swimming"];
@@ -65,7 +69,7 @@ export default function Maps({
           mapRegion.latitude
         }%2C${mapRegion.longitude}&radius=10000&keyword=${keyWords
           .map((word) => encodeURIComponent(word))
-          .join("%7C")}&key=AIzaSyBOGdOyuw2M85OMlkrTTDC1j3pYrR6XGfc`;
+          .join("%7C")}&key=${apiKey}`;
 
         const response = await fetch(apiUrl);
         const responseData = await response.json();
@@ -104,7 +108,7 @@ export default function Maps({
           listViewDisplayed="auto"
           fetchDetails={true}
           query={{
-            key: "AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY",
+            key: apiKey,
             language: "en",
           }}
           onPress={(data, details = null) => {
@@ -122,47 +126,36 @@ export default function Maps({
           showsMyLocationButton={true}
           showsCompass={true}
         >
-          {places.map((place) => (
-            <Marker
-              tappable={true}
-              key={place.place_id}
-              coordinate={{
-                latitude: place.geometry.location?.lat,
-                longitude: place.geometry.location?.lng,
-              }}
-              title={place.name}
-              description={place.vicinity}
-              pinColor="#00171F"
-            />
-          ))}
+          {places.length > 0 &&
+            places.map((place) => (
+              <Marker
+                tappable={true}
+                key={place.place_id}
+                coordinate={{
+                  latitude: place.geometry.location.lat,
+                  longitude: place.geometry.location.lng,
+                }}
+                title={place.name}
+                description={place.vicinity}
+                pinColor="#00171F"
+              />
+            ))}
         </MapView>
-
-        {/* <Searchbar
-        clearButtonMode="always"
-        style={styles.searchbar}
-        iconColor="white"
-        placeholderTextColor={"white"}
-        inputStyle={{ color: "white" }}
-        mode="bar"
-        placeholder="Search"
-        inputContainerStyle={{ backgroundColor: "#00171F", borderRadius: 20 }}
-        onTextInput={(query) => onChangeSearch(query)}
-        value={searchQuery}
-        icon={() => (
-          <TouchableOpacity onPress={toggleBottomSheet}>
-            <Image
-              source={require("../Images/burgermenu.png")}
-              style={{
-                width: 30,
-                height: 30,
-                alignSelf: "center",
-                right: 0,
-              }}
-            />
-          </TouchableOpacity>
-        )}
-      /> */}
       </View>
+      {/* <Searchbar
+        placeholder="Search"
+        onChangeText={onChangeSearch}
+        value={searchQuery}
+        style={{
+          position: "absolute",
+          alignSelf: "center",
+          top: 60,
+          width: "70%",
+          height: 50,
+          backgroundColor: "#00171F",
+          zIndex: 1,
+        }}
+      /> */}
       <View>
         <MapCarousel
           onCarouselItemChange={handleCarouselItemChange}
@@ -181,13 +174,5 @@ const styles = StyleSheet.create({
   map: {
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height,
-  },
-  searchbar: {
-    alignSelf: "center",
-    position: "absolute",
-    backgroundColor: "white",
-    top: 60,
-    width: "80%",
-    backgroundColor: "#00171F",
   },
 });
